ca214145f063e3d4a5c2ec52cb0a18224bd31fe3
Gabriel Maglia <93683370+gabiMaglia@users.noreply.github.com>
2024-04-25 09:09:03 -0300

    pre

--- Api/src/middleware/tokenAuthMiddlewares.js
+++ Api/src/middleware/tokenAuthMiddlewares.js
@@ -1,36 +1,44 @@
 const errors = require("../utils/errors");
-const { extractJwtToken, verifyToken, checkWhiteListedToken } = require("../utils/jwt/tokenGenerator");
-
+const {
+  extractJwtToken,
+  verifyToken,
+  checkWhiteListedToken,
+} = require("../utils/jwt/tokenGenerator");
 
 // MIDDLEWARE QUE CHEKEA TOKEN
 const checkAuthToken = async (req, res, next) => {
   try {
     if (!req.headers.authorization) throw new Error(errors.auth.unauthorized);
 
+    const token = extractJwtToken(req.headers.authorization);
 
-    const token = extractJwtToken(req.headers.authorization)
-
-    const isTokenListed = await checkWhiteListedToken(token)
+    const isTokenListed = await checkWhiteListedToken(token);
     if (!isTokenListed) throw new Error("Wrong Credentials");
 
-    const tokenData = await verifyToken(token)
-    if (!tokenData.id || !tokenData.role) throw new Error(errors.auth.unauthorized);
-   
-    req.userId = tokenData.id
-    req.role   = tokenData.role
-    next()
-    
-} catch (error) {
-  next(error)
-}
+    const tokenData = await verifyToken(token);
+    if (!tokenData.id || !tokenData.role)
+      throw new Error(errors.auth.unauthorized);
+
+    req.userId = tokenData.id;
+    req.role = tokenData.role;
+    next();
+  } catch (error) {
+    next(error);
+  }
 };
 
 // MIDDLEWARE QUE CHEKEA ROL
-const checkRoleAuthToken = (role) => async (req, res, next) => {
-
+const checkAdminAuthToken = (role) => async (req, res, next) => {
+  try {
+    if (!req.role || req.role !== "admin")
+      throw new Error(errors.auth.unauthorized);
+    next();
+  } catch (error) {
+    next(error);
+  }
 };
 
 module.exports = {
   checkAuthToken,
-  checkRoleAuthToken,
+  checkAdminAuthToken,
 };
